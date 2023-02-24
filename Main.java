ba815230c11a6ef7ff633037db4dcc7c532482f3
Gayathri-Inturi <120301218+Gayathri-Inturi@users.noreply.github.com>
2023-02-24 23:10:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Sample
+public class Main
  {
     
     public static void main(String[] args) {
